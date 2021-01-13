03b9a9a0b6a052c4d00b80537672f140da02f6ad
Thanapolseekhot <thanapol3012@gmail.com>
2021-01-13 10:47:30 +0700

    crate test.php

---
+++ testFor.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center>
+    <?php
+        for($i = 1 ; $i<=7 ; $i++){
+            echo ("<font size=\"".$i."\">welcom to php world</font><br />");
+            //echo ("font size=\"".$i."\">welcom to php world</font><br/>");
+        }
+    ?>
+    </center>
+</body>
+</html>
